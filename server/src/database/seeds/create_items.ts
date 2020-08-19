291023716d0687bf082e2b6b527a4ed9698f7dc3
Pedro <p.paiva.pdo@gmail.com>
2020-08-19 14:33:09 -0300

    inseridos os svg's corretos de ícones :hammer:

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -2,11 +2,11 @@ import Knex from 'knex'
 
 export async function seed(knex: Knex){
     await knex('items').insert([
-        {title: 'Lampadas', image:'eco.svg'},
-        {title: 'Pilhas e Baterias', image:''},
-        {title: 'Papéis e Papelão', image:''},
-        {title: 'Resíduos Eletrônicos', image:''},
-        {title: 'Resíduos Orgânicos', image:''},
-        {title: 'Óleo de cozinha', image:''},
+        {title: 'Lampadas', image:'lampadas.svg'},
+        {title: 'Pilhas e Baterias', image:'baterias.svg'},
+        {title: 'Papéis e Papelão', image:'papeis-papelao.svg'},
+        {title: 'Resíduos Eletrônicos', image:'eletronicos.svg'},
+        {title: 'Resíduos Orgânicos', image:'organicos.svg'},
+        {title: 'Óleo de cozinha', image:'oleo.svg'},
     ])
 } 
